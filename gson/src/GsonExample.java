0b028c643e3e2b051043c263e34d7a90554b6dcc
Valdir Stumm Júnior <stummjr@gmail.com>
2014-05-21 20:13:48 -0300

    finalizado tutorial de gson

--- gson/src/GsonExample.java
+++ gson/src/GsonExample.java
@@ -6,9 +6,12 @@ public class GsonExample {
     public static void main(String[] args) {
         Pessoa p = new Pessoa("João da Silva Sauro", 20, 75.2, 1.81);
         Gson gson = new Gson();
-        
+
         String json = gson.toJson(p);
-        
         System.out.println(json);
+        Pessoa p2 = gson.fromJson(json, Pessoa.class);
+        System.out.println(p2.getNome());
+
     }
+
 }
